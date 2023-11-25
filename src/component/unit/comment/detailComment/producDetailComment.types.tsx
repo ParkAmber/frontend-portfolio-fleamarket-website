f605e312c577aafbbabbc5192a05d43a15911536
Amber_Park <106687981+ParkAmber@users.noreply.github.com>
2023-11-24 23:20:55 -0500

    Update producDetailComment.types.tsx

--- src/component/unit/comment/detailComment/producDetailComment.types.tsx
+++ src/component/unit/comment/detailComment/producDetailComment.types.tsx
@@ -6,22 +6,7 @@ import {
   FormEvent,
 } from "react";
 import { FieldValues, SubmitHandler } from "react-hook-form";
-// export interface IProductListUIProps {
-//   isClicked: string;
-//   setIsClicked: Dispatch<SetStateAction<string>>;
-//   onChangeSearch: (e: ChangeEvent<HTMLInputElement>) => void;
-//   onClickSearch: () => void;
-//   dataBoards: any;
-//   onClickMoveToDetail: (e: MouseEvent<HTMLDivElement>) => void;
-//   starPoint: number[];
-//   onClickPrevPage: () => void;
-//   startPage: number;
-//   lastPage: number;
-//   onClickPage: (e: MouseEvent<HTMLButtonElement>) => void;
-//   onClickNextPage: () => void;
-//   isClickedpage: number;
-//   keyword: string;
-// }
+
 type Formdata = {
   contents: string;
 };
