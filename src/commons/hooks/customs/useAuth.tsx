da3b4bc618ce4ce3d3ee76220d15d89d74d54ebc
Amber_Park <106687981+ParkAmber@users.noreply.github.com>
2023-11-24 23:17:47 -0500

    Update useAuth.tsx

--- src/commons/hooks/customs/useAuth.tsx
+++ src/commons/hooks/customs/useAuth.tsx
@@ -13,13 +13,6 @@ export const useAuth = (): void => {
   const [accessToken, setAccessToken] = useRecoilState(accessTokenState);
 
   useEffect(() => {
-    // 1. 기존방식(refreshToken 이전)
-    // console.log("지금은 브라우저다!!!!!");
-    // const result = localStorage.getItem("accessToken");
-    // console.log(result);
-    // if (result) setAccessToken(result);
-
-    // 2. 새로운방식(refreshToken 이후) - 새로고침 이후에도 토큰 유지할 수 있도록
     void getAccessToken().then((newAccessToken) => {
       console.log(newAccessToken);
       // setAccessToken(newAccessToken ?? "");
