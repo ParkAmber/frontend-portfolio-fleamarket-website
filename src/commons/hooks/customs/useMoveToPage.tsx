e20d5d343c2ed4946417a8c7ee6c926ec221ebb1
Amber_Park <106687981+ParkAmber@users.noreply.github.com>
2023-11-24 23:18:05 -0500

    Update useMoveToPage.tsx

--- src/commons/hooks/customs/useMoveToPage.tsx
+++ src/commons/hooks/customs/useMoveToPage.tsx
@@ -13,15 +13,13 @@ export const useMoveToPage = (): IUseMoveToPageReturn => {
   const [visitedPage, setVisitedPage] = useRecoilState(visitedPageState);
 
   const onClickMoveToPage = (path: string) => (event: any) => {
-    //방법1
-    setVisitedPage(path); //login page일때는 setVisitedPage하지 않도록 조건문 만들어주기
-    //방법2
-    // localStorage.setItem("visitedPage", path); //로컬스토리지 이용해서 주소 저장 가능!
+
+    setVisitedPage(path); 
+
     void router.push(path);
   };
 
   return {
-    visitedPage, //==>오브젝트이므로 {key: value} 형식! =>원래는 { visitedPage: visitedPage, onClickMoveToPage: onClickMoveToPage } 임!!
-    onClickMoveToPage, //==> onClickMoveToPage: onClickMoveToPage ==> shorthand-property
-  };
+    visitedPage,
+    onClickMoveToPage, 
 };
