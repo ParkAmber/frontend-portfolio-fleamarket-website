9b3672d6cb98eefa16f07b06827f43c591699510
Amber_Park <106687981+ParkAmber@users.noreply.github.com>
2023-11-24 23:21:42 -0500

    Update producDetailCommentFetch.types.tsx

--- src/component/unit/comment/detailCommentFetch/producDetailCommentFetch.types.tsx
+++ src/component/unit/comment/detailCommentFetch/producDetailCommentFetch.types.tsx
@@ -1,20 +1,5 @@
 import { MouseEvent, ChangeEvent, SetStateAction, Dispatch } from "react";
-// export interface IProductListUIProps {
-//   isClicked: string;
-//   setIsClicked: Dispatch<SetStateAction<string>>;
-//   onChangeSearch: (e: ChangeEvent<HTMLInputElement>) => void;
-//   onClickSearch: () => void;
-//   dataBoards: any;
-//   onClickMoveToDetail: (e: MouseEvent<HTMLDivElement>) => void;
-//   starPoint: number[];
-//   onClickPrevPage: () => void;
-//   startPage: number;
-//   lastPage: number;
-//   onClickPage: (e: MouseEvent<HTMLButtonElement>) => void;
-//   onClickNextPage: () => void;
-//   isClickedpage: number;
-//   keyword: string;
-// }
+
 export interface IProductDetailUIProps {
   data: any;
   onClickListMove: (e: MouseEvent<HTMLButtonElement>) => void;
