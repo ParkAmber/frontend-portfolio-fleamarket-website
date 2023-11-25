de44b36041779c4de9c2a37a44de32c9a59b86eb
Amber_Park <106687981+ParkAmber@users.noreply.github.com>
2023-11-24 23:20:33 -0500

    Update 02-after.validation.ts

--- src/component/libraries/02-after.validation.ts
+++ src/component/libraries/02-after.validation.ts
@@ -9,20 +9,4 @@ export const schema = yup.object({
   useditemAddress: yup.object().shape({
     address: yup.string().required("Address is required"),
   }),
-  // //이메일 검증
-  // email: yup
-  //   .string()
-  //   .email("email형식에 적합하지 않음")
-  //   .required("email를 입력해주세여"),
-  // //비번 검증
-  // password: yup
-  //   .string()
-  //   .min(4, "please 4자리 이상으로 입력해라")
-  //   .max(15, "please 15자리 이하로 입력해라")
-  //   .required("password를 입력해주세여"),
-
-  // phone: yup
-  //   .string()
-  //   .matches(/^\d{3}-\d{3,4}-\d{4}$/, "형식에 맞게 입력해라")
-  //   .required("phone를 입력해주세여"),
 });
